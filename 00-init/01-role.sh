3c57b5fc2745a674975cc45c203c22be36b99772
awsandy <awsandy@amazon.co.uk>
2023-09-17 18:21:26 +0100

    up

--- 00-init/01-role.sh
+++ 00-init/01-role.sh
@@ -1,8 +1,9 @@
 aws iam create-role --path / \
 --role-name lattice-workshop-role \
 --description "Role used by VPC Lattice Cloud9 environment" \
---assume-role-policy-document "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Service\":[\"ec2.amazonaws.com\"]},\"Action\":[\"sts:AssumeRole
-\"]}]}"
+--assume-role-policy-document file:///trust.json
+
+
 
 aws iam attach-role-policy --role-name lattice-workshop-role --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
 
