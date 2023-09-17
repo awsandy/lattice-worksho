3cfbcc9e4250d3979348679e793ab62ed5521928
awsandy <awsandy@amazon.co.uk>
2023-09-17 18:45:43 +0100

    up

--- 00-init/05-stack.sh
+++ 00-init/05-stack.sh
@@ -1,8 +1,8 @@
-source ./00-env.sh
+
 curl 'https://static.us-east-1.prod.workshops.aws/public/d03a4195-d09c-405e-b4c3-b46fd34923de/static/code/LatticeBaseInfraWithAPIServer.yaml' --output LatticeBaseInfraWithAPIServer.yaml
 
-aws --region "${AWS_REGION}" \
+aws  \
     cloudformation deploy \
-    --stack-name "${ENVIRONMENT_NAME}latticebaseinfrawithapiserver" \
+    --stack-name "latticebaseinfrawithapiserver" \
     --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --template-file "./LatticeBaseInfraWithAPIServer.yaml"
