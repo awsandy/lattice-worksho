3902b8caf13d606948167d2b7d8ff7b53a59f014
awsandy <awsandy@amazon.co.uk>
2023-10-06 08:13:15 +0100

    up

--- demo/lab2-local.sh
+++ demo/lab2-local.sh
@@ -11,7 +11,12 @@ snid=$(aws vpc-lattice list-service-networks | jq -r '.items[] | select(.name=="
 aws vpc-lattice update-service-network --auth-type AWS_IAM --service-network-identifier $snid
 psid=$(aws vpc-lattice list-services | jq -r '.items[] | select(.name=="parking").id')
 aws vpc-lattice update-service --auth-type AWS_IAM --service-identifier $psid
+#
+
 
 # NONE
 snid=$(aws vpc-lattice list-service-networks | jq -r '.items[] | select(.name=="superappsvcnetwork").id')
 aws vpc-lattice update-service-network --auth-type NONE --service-network-identifier $snid
+psid=$(aws vpc-lattice list-services | jq -r '.items[] | select(.name=="parking").id')
+aws vpc-lattice update-service --auth-type NONE --service-identifier $psid
+#
