f0636304b0401950e09ecd5d38118207625cd999
awsandy <awsandy@amazon.co.uk>
2023-11-23 21:43:30 +0000

    up

--- lab4/null_auth.tf
+++ lab4/null_auth.tf
@@ -12,9 +12,7 @@ resource "null_resource" "gen_cluster_auth" {
         CLUSTER_NAME=$(echo ${var.CLUSTER1_NAME})
         echo "Cluster name = $CLUSTER_NAME"
         eksctl utils write-kubeconfig --cluster $CLUSTER_NAME
-        context=$(kubectl config get-contexts -o name | grep $CLUSTER_NAME)
-        kubectl config rename-context $context $CLUSTER_NAME
-        kubectl --context $CLUSTER_NAME version --short && kubectl --context "$CLUSTER_NAME" get nodes
+        kubectl  version --short && kubectl get nodes
      EOT
   }
 }
