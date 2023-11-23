73c7c15bfbc0edfab63e234d2dee7d886deed8a4
awsandy <awsandy@amazon.co.uk>
2023-11-23 19:43:29 +0000

    up

--- init/ssh-key.tf
+++ init/ssh-key.tf
@@ -4,13 +4,13 @@ resource "tls_private_key" "local" {
 }
 
 resource "local_file" "ssh_private_key" {
-  content         = tls_private_key.local.private_key_ssh
+  content         = tls_private_key.local.private_key_openssh
   filename        = "/home/ec2-user/.ssh/id_rsa"
   file_permission = "0400"
 }
 
 resource "local_file" "ssh_public_key" {
-  content         = tls_private_key.local.public_key_ssh
+  content         = tls_private_key.local.public_key_openssh
   filename        = "/home/ec2-user/.ssh/id_rsa.pub"
   file_permission = "0400"
 }
