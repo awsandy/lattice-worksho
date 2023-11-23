2c81dcf9f35ec87fde69c35d95942d1417a8f82a
awsandy <awsandy@amazon.co.uk>
2023-11-23 21:49:27 +0000

    up

---
+++ c9-setup/auth-console.sh
@@ -0,0 +1,12 @@
+# auth console:
+c9builder=$(aws cloud9 describe-environment-memberships --environment-id=$C9_PID | jq -r '.memberships[].userArn')
+if echo ${c9builder} | grep -q user; then
+	rolearn=${c9builder}
+        echo Role ARN: ${rolearn}
+elif echo ${c9builder} | grep -q assumed-role; then
+        assumedrolename=$(echo ${c9builder} | awk -F/ '{print $(NF-1)}')
+        rolearn=$(aws iam get-role --role-name ${assumedrolename} --query Role.Arn --output text) 
+        echo Role ARN: ${rolearn}
+fi
+cn=$(aws eks list-clusters --query clusters --output text)
+eksctl create iamidentitymapping --cluster $cn --arn ${rolearn} --group system:masters --username admin
